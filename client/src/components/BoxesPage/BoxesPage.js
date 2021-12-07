857d1aa73ce20d9abc0222111ed6f001ed91977f
Anastasia K <anastasia.khomchenko@gmail.com>
2021-12-07 15:39:02 +0300

    filter logic done, seed updated

--- client/src/components/BoxesPage/BoxesPage.js
+++ client/src/components/BoxesPage/BoxesPage.js
@@ -21,21 +21,10 @@ export const BoxesPage = () => {
           <div className="flex flex-wrap -mx-4 justify-center">
           <FilterNav />
             {boxes.map((el) => {
-              console.log(el)
               return (
                 <Box
                 el={el}
-                // key={el.id}
-                // id={el.id}
-                // img={el.Store.store_img}
-                // restName={el.Store.name}
-                // boxName={el.name}
-                // descr={el.descr}
-                // count={el.count}
-
-                // price={el.price}
-                // start_date={el.start_date}
-                // end_date={el.end_date}
+                key={el.id}
                 />
               )
             })}
