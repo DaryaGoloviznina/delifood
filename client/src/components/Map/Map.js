45fb842035692db6f29528326e7efd54972ea151
Vlad Milko <vladmilko27@gmail.com>
2021-12-08 14:55:26 +0600

    Add Placemarks to Map

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -2,57 +2,60 @@ import { useEffect, useState } from 'react';
 import { YMaps, Map, SearchControl} from 'react-yandex-maps';
 import { useNavigate } from 'react-router-dom';
 import {UserPlacemark} from './PlaceMark'
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllBoxesThunk } from '../../store/boxes/actions';
+import { getUserLocationThunk } from '../../store/user/clientLocation/actions';
+import BoxModal from '../BoxesPage/boxModal/BoxModal';
 
 export const RestMap = () => {
   
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const boxes = useSelector((store) => (store.boxes?.boxes));
+  const user = useSelector((store) => (store.auth?.user));
 
-  function buttonHandler () {
-    navigate('/');
+  const [showModal, setShowModal] = useState(false);
+  const [boxData, setBoxdata] = useState({});
+
+  function buttonHandler(box) {
+    setBoxdata(box);
+    setShowModal(true);
   }
 
-  const [lon, SetLon] = useState(0);
-  const [lat, SetLat] = useState(0);
-  const [address, SetAddres] = useState(0);
-
-  useEffect(async () => {
-    navigator.geolocation.getCurrentPosition(position => {
-      const { latitude, longitude } = position.coords;
-      SetLon(longitude);
-      SetLat(latitude);
-    });
-
-    let req = await fetch(`https://geocode-maps.yandex.ru/1.x/?format=json&apikey=51d9c7fc-7e81-4f44-a747-14323b05f7a6&geocode=${lon}, ${lat}`)
-    let res = await req.json();
-    SetAddres(res.response.GeoObjectCollection.featureMember[0].GeoObject.metaDataProperty.GeocoderMetaData.Address.formatted);
-  }, [])
+  useEffect(() => {
+    dispatch(getAllBoxesThunk());
+    if (user && !user.location) dispatch(getUserLocationThunk());
+  }, []);
 
   return (
-    <div
-      className='container lg'
-    >
-      <p>Address: <b>{address}</b></p>
+    <div className="container mx-auto">
+      <BoxModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        boxData={boxData} 
+      />
       <YMaps 
         query={{apikey: 'a9e98eaf-d4c4-45e6-9ee4-5afad392d357'}}
       >
-        <Map defaultState={{ center: [lat, lon], zoom: 9 }} width={'100%'} height={'600px'} options={{autoFitToViewport: 'always'}} modules={["geolocation", "geocode"]} >
-          <UserPlacemark 
-            geometry={[lat, lon]}
-            options={{
-              iconColor: '#ff0000',
-              hideIconOnBalloonOpen: false,
-              balloonMaxWidth: 200,
-            }}
-            myClick={() => buttonHandler()} 
-            user={{id: 0}}
-          />
+        <Map state={{ center: [user?.location?.lat, user?.location?.lon], zoom: 9 }} width={'100%'} height={'600px'} options={{autoFitToViewport: 'always'}} modules={["geolocation", "geocode"]} >
+          {boxes.map((el) => {
+            <UserPlacemark 
+              geometry={[el.store_lat, el.store_lon]}
+              options={{
+                iconColor: '#ff0000',
+                hideIconOnBalloonOpen: false,
+                balloonMaxWidth: 200,
+              }}
+              myClick={() => buttonHandler(el)} 
+              user={{id: 0}}
+            />
+          })}
           <SearchControl options={{ float: 'right' }} onResultSelect={async (e) => {
-            const index = e.get('index');
-            e.originalEvent.target.getResult(index)
-              .then((res) => console.log(res.geometry.getCoordinates()));
-            SetAddres(e.originalEvent.target.getRequestString());
+            // const index = e.get('index');
+            // e.originalEvent.target.getResult(index)
+            //   .then((res) => console.log(res.geometry.getCoordinates()));
+            // SetAddres(e.originalEvent.target.getRequestString());
           }}/>
-        {/* {rests.map((el, ind) => <Placemark key={ind} geometry={el} />)} */}
         </Map>
       </YMaps>
     </div>
